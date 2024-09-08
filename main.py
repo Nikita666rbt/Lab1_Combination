836fc39594c9848183ee840b82f7bead46654181
Никита Литовка <nikituslitus@yandex.ru>
2024-09-08 18:07:25 +0300

    Написание функции и тест

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-#def Combination()
+def Combination(listNumbers):
+    for i in range(len(listNumbers)-2):
+        list123 = listNumbers[i:i + 3]
+        if sorted(list123) == [1,2,3]:
+            print("True")
+        else:
+            print("False")
 
-n = 0
-n = input("Введите n: ")
-print(n)
+
+n = int(input("Введите длину списка n: "))
+listNumbers = list(map(int, input("Введите элементы списка[",n,"]").split()))
+
+print("Результат:", Combination(listNumbers))
