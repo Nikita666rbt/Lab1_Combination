e96b30b8663611e383064072617dabc94ca2b939
Никита Литовка <nikituslitus@yandex.ru>
2024-09-08 18:20:57 +0300

    Переделан вывод

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ def Combination(listNumbers):
     for i in range(len(listNumbers)-2):
         list123 = listNumbers[i:i + 3]
         if sorted(list123) == [1,2,3]:
-            print("True")
-        else:
-            print("False")
-
+            return True
+    return False
 
 n = int(input("Введите длину списка n: "))
 listNumbers = list(map(int, input("Введите элементы списка: ").split()))
@@ -13,5 +11,5 @@ listNumbers = list(map(int, input("Введите элементы списка:
 if len(listNumbers) != n:
     print("Ошибка: количество введенных элементов не соответствует заданной длине списка.")
 else:
-    print("Результат:", Combination(listNumbers))
+    print("Результат: ", Combination(listNumbers))
 
